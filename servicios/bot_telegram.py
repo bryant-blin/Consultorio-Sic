5033c1d01451b8ad35a30b83214cc1d90331df11
Bryant Blin <bryantbueno222@gmail.com>
2026-04-24 11:59:20 -0400

    Corrigiendo import de timedelta

--- servicios/bot_telegram.py
+++ servicios/bot_telegram.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-from datetime import datetime
+from datetime import datetime, timedelta
 import threading
 import os
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
@@ -276,8 +276,9 @@ def iniciar_bot_sic(app, db, Cita, Historial_Medico):
                 fecha_sel = user_states[chat_id]['datos']['fecha']
                 obj_fecha_hora = datetime.strptime(f"{fecha_sel} {hora_final}", "%Y-%m-%d %H:%M")
                 
-                # Render usa UTC, ajustamos a horario de Venezuela (-4h)
-                ahora_venezuela = datetime.now() - timedelta(hours=4)
+               ahora_venezuela = datetime.now() - timedelta(hours=4)
+print(f"DEBUG: Cliente eligio {obj_fecha_hora} | Servidor cree que es {ahora_venezuela}")
+
                 
                 if obj_fecha_hora < ahora_venezuela:
                     bot.answer_callback_query(call.id, "⚠️ Hora inválida") # Detener el círculo de carga
